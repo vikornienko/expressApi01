a16de264917298f0e57c7a40b54e5b9b6e2cf0ab
vikornienko <vikornienko76@gmail.com>
2024-11-16 10:24:00 +0500

    Was adding routs for users.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 
+import { userRouter } from './users/users.js'; 
+
 
 const port = 8000;
 const app = express();
 
 app.get('/hello', (req, res) => {
     res.send('Hello!')
-})
+});
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`The server start on http://localhost:${port}`)
